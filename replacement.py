c2c0a048bbd08d5ee78e5de37704f6b448f5d64e
as.artur@gmail.com <Barabasz32#>
2020-03-06 10:23:30 +0100

    thurs

--- replacement.py
+++ replacement.py
@@ -9,6 +9,10 @@ new_tag.text = 'body text'
 new_tag.attrib['x'] = '1' # must be str; cannot be an int
 new_tag.attrib['y'] = 'abc'
 
+new_tag.text = 'body text'
+new_tag.attrib['x'] = '1' # must be str; cannot be an int
+new_tag.attrib['y'] = 'abc'
+
 
 
 # Write back to file
